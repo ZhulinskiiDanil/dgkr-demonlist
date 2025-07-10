851afe176e6d5411d2884b7ab2af22169df48b1d
ZhulinskiiDanil <zhulinskiidanil@gmail.com>
2025-07-10 23:27:56 +0200

    chore: fuse.js search at demonlist

--- src/widgets/DGKRList/ui/index.tsx
+++ src/widgets/DGKRList/ui/index.tsx
@@ -1,7 +1,10 @@
 'use client';
 import styles from './DGKRList.module.scss';
 
+import Fuse from 'fuse.js';
+
 import { DGKRListLevel } from '../DGKRListLevel/ui';
+import { useState } from 'react';
 import { useDGKRListQuery } from '@/shared/hooks/useDGKRListQuery';
 import { useDemonlistQuery } from '@/shared/hooks/useDemonlistQuery';
 
@@ -9,39 +12,88 @@ export function DGKRList() {
   const { data: levelsList } = useDGKRListQuery();
   const { data: demonlist } = useDemonlistQuery();
 
+  const [query, setQuery] = useState('');
+
   function getDemonlistLevelById(id: string | number) {
     return demonlist.find((level) => level.level_id === id) || null;
   }
 
-  // Sort levels by place
-  const sortedLevels = levelsList?.sort((a, b) => {
-    const aLevel = getDemonlistLevelById(a.levelId);
-    const bLevel = getDemonlistLevelById(b.levelId);
+  const parsedLevels = levelsList
+    .map((level) => ({
+      level,
+      listmeta: demonlist.find(
+        (demonlistLevel) => demonlistLevel.level_id === level.levelId
+      ),
+    }))
+    .filter(
+      (
+        elm
+      ): elm is NonNullable<typeof elm> & {
+        listmeta: NonNullable<typeof elm.listmeta>;
+      } => !!elm.listmeta
+    );
 
-    if (!aLevel || !bLevel) return 0;
-    const placeA = aLevel.place || 0;
-    const placeB = bLevel.place || 0;
-    return placeA - placeB;
+  // Sort levels by place
+  const sortedLevels =
+    parsedLevels?.sort((a, b) => {
+      const aLevel = getDemonlistLevelById(a.level.levelId);
+      const bLevel = getDemonlistLevelById(b.level.levelId);
+
+      if (!aLevel || !bLevel) return 0;
+      const placeA = aLevel.place || 0;
+      const placeB = bLevel.place || 0;
+      return placeA - placeB;
+    }) || [];
+
+  // Настройки Fuse.js
+  const fuse = new Fuse(sortedLevels, {
+    keys: [
+      'level.name',
+      'listmeta.creator',
+      'listmeta.verifier',
+      'level.victors.victorName',
+    ],
+    threshold: 0.3, // чувствительность (меньше = строже)
   });
 
+  // Если есть query - используем Fuse.js, иначе оставляем отсортированный список
+  const filteredLevels = query
+    ? fuse.search(query).map((result) => result.item)
+    : sortedLevels;
+
   return (
-    <ul className={styles.list}>
-      {(sortedLevels || [])
-        .map((level, index) => {
-          const demonlistLevel = getDemonlistLevelById(level.levelId);
+    <div className={styles.listWrapper}>
+      <div className={styles.search}>
+        <p className={styles.title}>Поиск по списку</p>
+        <input
+          type="text"
+          placeholder="Поиск по имени, создателю, вериферу..."
+          className={styles.searchInput} // добавь стили для инпута в DGKRList.module.scss
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        {!filteredLevels.length && (
+          <p className={styles.sub}>По запросу "{query}" ничего найдено</p>
+        )}
+        {query.length > 1 && filteredLevels.length > 0 && (
+          <p className={styles.sub}>
+            По запросу "{query}" найдено {filteredLevels.length} элементов
+          </p>
+        )}
+      </div>
 
-          if (!demonlistLevel) return;
-
+      <ul className={styles.list}>
+        {filteredLevels.map((level, index) => {
           return (
             <DGKRListLevel
-              key={level.levelId}
-              data={level}
-              demonlistLevel={demonlistLevel}
+              key={level.level.levelId}
+              data={level.level}
+              demonlistLevel={level.listmeta}
               place={index + 1}
             />
           );
-        })
-        .filter(Boolean)}
-    </ul>
+        })}
+      </ul>
+    </div>
   );
 }
