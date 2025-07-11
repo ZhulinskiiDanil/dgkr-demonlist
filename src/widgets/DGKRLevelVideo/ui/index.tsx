260d1f5d3ed231130b9754a44b511160407bad21
ZhulinskiiDanil <zhulinskiidanil@gmail.com>
2025-07-12 01:26:01 +0200

    fix: fix youtube embed

--- src/widgets/DGKRLevelVideo/ui/index.tsx
+++ src/widgets/DGKRLevelVideo/ui/index.tsx
@@ -1,18 +1,21 @@
 'use client';
 
+import { getYoutubeVideoId } from '@/shared/utils/getYoutubeVideoId';
 import styles from './DGKRLevelVideo.module.scss';
 
 import type { DGKRVictor } from '@/shared/types/demonlist';
 
 export function DGKRLevelVideo({ victor }: { victor: DGKRVictor }) {
   const videoUrl = victor?.videoUrl || '';
+  const videoId = getYoutubeVideoId(videoUrl);
+  const embed = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
 
   return (
     <main className={styles.container}>
       {videoUrl && (
         <div className={styles.videoWrapper}>
           <iframe
-            src={videoUrl.replace('watch?v=', 'embed/') + '?autoplay=1'}
+            src={embed}
             style={{ border: 'none' }}
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
